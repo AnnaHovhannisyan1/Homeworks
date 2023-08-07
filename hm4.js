38b85f7d17c4cc9552b21df4ddc8e93c6ffe8c03
AnnaHovhannisyan1 <138381155+AnnaHovhannisyan1@users.noreply.github.com>
2023-08-07 14:15:15 +0400

    Create hm4.js

---
+++ hm4.js
@@ -0,0 +1,49 @@
+// 1.Array includes 
+
+// function arrIncludes(arr, item) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (item === arr[i]) {
+//             return true
+//         }
+//     }
+//     return false
+// }
+// let res = arrIncludes([1, 2, 3, 4, 5, 6], 1) // true
+// console.log(res)
+
+//2. String includes
+// function strIncludes(str, item) {
+//     for (let i = 0; i < str.length; i++) {
+//         if (item === str[i]) {
+//             return true
+//         }
+//     }
+//     return false
+// }
+// let res = strIncludes('Anna', 'n') 
+// console.log(res)
+
+// 3. Array indexOf
+// function arrIndexOF(arr, index) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (index === arr[i]) {
+//             return i
+//         }
+//     }
+// return -1
+// }
+// let res = arrIndexOF([1, 2, 3, 4, 5, 6], 10) // -1
+// console.log(res)
+
+// 4. String indexOf 
+// function indexOf(str, element, from) {
+//     for (let i = from; i < str.length; i++) {
+//         if (str[i] === element) {
+//             return i 
+//         }
+        
+//     }
+//     return -1
+// }
+//let res = indexOf('Anna', 'a', 1) 
+// console.log(res)
